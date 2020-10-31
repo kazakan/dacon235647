d28cc7de4f685c5a6e816cabe7cea931cbe234c3
Jh Do <dojh99@gmail.com>
2020-10-31 20:42:48 +0900

    Changes to be committed: 	modified:   work.ipynb 	modified:   work.py

--- work.py
+++ work.py
@@ -1,18 +1,36 @@
 
-# %%
 '''
 Download data and set directory.
 '''
 
 # %%
-!mkdir workplace
-!cd workplace
-
 !wget 'https://drive.google.com/uc?export=download&id=192fXQDhp84rtmb3HXvhd7TTpVeDOHv4P' -O data.zip
 !unzip data.zip -d data
 
 !ls
 
 # %%
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+import seaborn
+
+PATH_TRAIN = "./data/train.csv"
+PATH_TEST_X = "./data/test_x.csv"
+
+'''
+Lets See train data
+'''
+
+# %%
+#load data
+df_train = pd.read_csv(PATH_TRAIN)
+
+# %%
+df_train.top(10)
+
+# %%
+df_train.describe()
+
 
 
