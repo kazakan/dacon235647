0a584579c5767300c5b6cb61d9e242c3e36944a9
Jh Do <dojh99@gmail.com>
2020-10-31 20:18:33 +0900

    Initial commit

---
+++ work.py
@@ -0,0 +1,18 @@
+
+# %%
+'''
+Download data and set directory.
+'''
+
+# %%
+!mkdir workplace
+!cd workplace
+
+!wget 'https://drive.google.com/uc?export=download&id=192fXQDhp84rtmb3HXvhd7TTpVeDOHv4P' -O data.zip
+!unzip data.zip -d data
+
+!ls
+
+# %%
+
+
